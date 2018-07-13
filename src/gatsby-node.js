31b9138f705f2ffb5189ce28c5a649e044c230e4
Trevis <trevis@flipstone.com>
2018-07-13 10:22:27 -0400

    Update for gatsby-v2 syntax, lost eslint plugin options

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,17 +1,21 @@
-exports.modifyWebpackConfig = ({ config, stage }, pluginOptions) => {
-  const test = pluginOptions.test || /\.js$|\.jsx$/;
-  const exclude = pluginOptions.exclude || /(node_modules|cache|public)/;
-  const options = pluginOptions.options || {};
+exports.onCreateWebpackConfig = ({ actions, getConfig, stage }, pluginOptions) => {
+  const config = getConfig();
 
-  if (stage === 'develop') {
-    config.loader('js|jsx', {
-      loader: 'eslint-loader',
-      test,
-      exclude,
-    });
-    config.merge({
-      eslint: options
-    });
-  }
-  return config;
+    if (stage === 'develop') {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: pluginOptions.test || /\.js$|\.jsx$/,
+                        exclude: pluginOptions.exclude || /(node_modules|cache|public)/,
+                        use: [
+                            {
+                                loader: 'eslint-loader'
+                            }
+                        ]
+                    }
+                ]
+            }
+        });
+    }
 };
